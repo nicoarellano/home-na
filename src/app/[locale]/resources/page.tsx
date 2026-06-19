b68533b307a16228d5a441665bbcc0bfd61e69bf
Nico <nico14@gmail.com>
2026-06-19 16:48:36 -0400

    refactor resources and Faq section

--- src/app/[locale]/resources/page.tsx
+++ src/app/[locale]/resources/page.tsx
@@ -1,10 +1,7 @@
-import { setRequestLocale, getTranslations } from 'next-intl/server'
+import { setRequestLocale } from 'next-intl/server'
 import { SiteChrome } from '@/components/home/SiteChrome'
-import PageHeader from '@/components/home/PageHeader'
-import PrinciplesSection from '@/components/home/PrinciplesSection'
-import TrustBandSection from '@/components/home/TrustBandSection'
+import DocsHeroSection from '@/components/home/DocsHeroSection'
 import PlatformArchitectureEmbed from '@/components/home/PlatformArchitectureEmbed'
-import DeveloperPlatformSection from '@/components/home/DeveloperPlatformSection'
 import FaqSection from '@/components/home/FaqSection'
 
 interface Props {
@@ -14,16 +11,12 @@ interface Props {
 export default async function ResourcesPage({ params }: Props) {
   const { locale } = await params
   setRequestLocale(locale)
-  const t = await getTranslations({ locale, namespace: 'HomePage.pageHeaders.resources' })
 
   return (
     <SiteChrome>
-      <PageHeader eyebrow={t('eyebrow')} title={t('title')} subtitle={t('subtitle')} />
-      <DeveloperPlatformSection />
+      <DocsHeroSection />
       <PlatformArchitectureEmbed />
-      <PrinciplesSection />
-      <TrustBandSection />
-      {/* <FaqSection /> */}
+      <FaqSection />
     </SiteChrome>
   )
 }
